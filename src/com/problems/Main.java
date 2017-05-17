92cad67bfa12171b0362a10b51b6c31016d05103
Yaxuan Zhang <zhangyaxuan310@gmail.com>
2017-05-17 18:51:13 +0800

    bubble sort

--- src/com/problems/Main.java
+++ src/com/problems/Main.java
@@ -1,28 +1,29 @@
 package com.problems;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        bubbleSort(new Integer[]{5, 9, 2, 1, 3, 6, 8, 7, 4, 0});
+        bubbleSort(new Integer[]{6, 9, 2, 0, 1, 8, 5, 4, 7, 3});
     }
 
+
     private static void bubbleSort(Integer[] numbers){
+        int times = 0;
         for(int i=0; i < numbers.length; i++){
-            int min = numbers[i];
-            for(int j = i+1; j < numbers.length; j++){
-                if(numbers[j] < min){
-                    min = numbers[j];
-                    numbers[j] = numbers[i];
-                    numbers[i] = min;
+            for(int j = numbers.length-1; j > i; j--){
+                if(numbers[j] < numbers[j-1]){
+                    int min = numbers[j];
+                    numbers[j] = numbers[j-1];
+                    numbers[j-1] = min;
+                    times++;
                 }
             }
         }
         System.out.println(Arrays.toString(numbers));
+        System.out.println("Bubble sort times:" + times);
     }
+
 }
