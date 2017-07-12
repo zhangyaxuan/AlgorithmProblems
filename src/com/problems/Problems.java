f1dd44dd9270e72d99fc7d7831b2525d98fbc4f7
Yaxuan Zhang <zhangyaxuan310@gmail.com>
2017-07-12 10:57:28 +0800

    solutions for algorithm problems on leetcode

---
+++ src/com/problems/Problems.java
@@ -0,0 +1,114 @@
+package com.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by mac on 06/07/2017.
+ */
+public class Problems {
+
+    public int lengthOfLongestSubstring(String s) {
+        int i=0, j=0, max=0;
+        List<Character> noRepeat = new ArrayList<Character>();
+        while(i < s.length()){
+            if(!noRepeat.contains(s.charAt(i))){
+                noRepeat.add(s.charAt(i++));
+                max = Math.max(max, noRepeat.size());
+            } else{
+                noRepeat.remove(s.charAt(j++));
+            }
+        }
+        return max;
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int i=0, j=0;
+        List<Integer> sortedList = new ArrayList<Integer>();
+        while(i < nums1.length || j < nums2.length){
+            if(i < nums1.length && j == nums2.length){
+                sortedList.add(nums1[i]);
+                i++;
+            } else if(j < nums2.length && i == nums1.length){
+                sortedList.add(nums2[j]);
+                j++;
+            } else if(nums1[i] < nums2[j]) {
+                sortedList.add(nums1[i]);
+                i++;
+            } else{
+                sortedList.add(nums2[j]);
+                j++;
+            }
+
+        }
+        int index  = sortedList.size()/2;
+        if(sortedList.size() > 2 && sortedList.size()%2 !=0){
+            return sortedList.get(index);
+        } else {
+            double sum = sortedList.get(index-1) + sortedList.get(index);
+            return sum/2;
+        }
+    }
+
+    public String longestPalindrome(String s) {
+
+        List<String> palindrome = new ArrayList<String>();
+        for(int m = 0; m<s.length(); m++){
+            explore(s, palindrome, m, m);
+            explore(s, palindrome, m, m+1);
+        }
+
+        String longest = "";
+        for(String str : palindrome){
+            if(str.length() > longest.length()){
+                longest = str;
+            }
+        }
+        return longest;
+    }
+
+    private void explore(String s, List<String> palindrome, int i, int j) {
+        while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)){
+            palindrome.add(s.substring(i, j+1));
+            i--;
+            j++;
+        }
+    }
+
+    public String zigzagConvert(String s, int numRows) {
+        if(numRows <=1) return s;
+        StringBuilder[] builders = new StringBuilder[numRows];
+        for(int i=0; i< builders.length; i++){
+            builders[i] = new StringBuilder("");
+        }
+        int index = 0;
+        int next = 1;
+        for(int i = 0; i<s.length(); i++){
+            builders[index].append(s.charAt(i));
+            if(index == 0) next = 1;
+            if(index == numRows -1) next = -1;
+            index += next;
+        }
+        String result = "";
+        for (int j = 0; j< builders.length; j++){
+            result += builders[j];
+        }
+        return result;
+    }
+
+    public int reverseInteger(int x) {
+        boolean isNagtive = (x < 0);
+        String xStr = String.valueOf(Math.abs(x));
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i< xStr.length(); i++){
+            stack.push(xStr.charAt(i));
+        }
+        String reversedStr = "";
+        while (!stack.empty()){
+            reversedStr += stack.pop();
+        }
+        int reversed = isNagtive ? (0-Integer.valueOf(reversedStr)) : Integer.valueOf(reversedStr);
+        return reversed;
+    }
+}
---
+++ test/com/problems/ProblemsTest.java
@@ -0,0 +1,53 @@
+package com.problems;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by mac on 06/07/2017.
+ */
+public class ProblemsTest {
+    Problems problems = new Problems();
+    @Test
+    public void shouldFindMedimNum(){
+        int[] nums1 = new int[]{1,2};
+        int[] nums2 = new int[]{3,4};
+
+        double result = problems.findMedianSortedArrays(nums1, nums2);
+        assertThat(result, is(2.5));
+    }
+
+    @Test
+    public void shouldFindLongestPalindrome(){
+        String s1 = "babad";
+        String s2 = "cbbd";
+        String result1 = problems.longestPalindrome(s1);
+        String result2 = problems.longestPalindrome(s2);
+        assertThat(result1, is("bab"));
+        assertThat(result2, is("bb"));
+    }
+
+    @Test
+    public void shouldConvertAsZigzagPatten(){
+        String testStr = "PAYPALISHIRING";
+        int rowNum = 3;
+        String result = problems.zigzagConvert(testStr, 3);
+        assertThat(result,is("PAHNAPLSIIGYIR"));
+    }
+
+    @Test
+    public void shouldReverseInteger(){
+        int origin1 = 123;
+        int origin2 = -123;
+//        int origin3 = 1000000003;
+        int reversed1 = problems.reverseInteger(origin1);
+        int reversed2 = problems.reverseInteger(origin2);
+//        int reversed3 = problems.reverseInteger(origin3);
+        assertThat(reversed1, is(321));
+        assertThat(reversed2, is(-321));
+//        assertThat(reversed3, is(0));
+    }
+
+}
